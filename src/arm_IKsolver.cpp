ed3919e5394f143c73d21c2023275c217b14f2ed
gjc13 <amtcmp@126.com>
2016-01-23 21:31:29 +0800

    rename output topic

--- src/arm_IKsolver.cpp
+++ src/arm_IKsolver.cpp
@@ -1,5 +1,5 @@
 #include "ros/ros.h"
-#include "tk_arm/outpos.h"
+#include "tk_arm/OutPos.h"
 #include <kdl/frames_io.hpp>
 #include <kdl/chainfksolverpos_recursive.hpp>
 #include <kdl/chainiksolverpos_lma.hpp>
@@ -7,7 +7,7 @@
 #include <iostream>
 #include <sstream>
 
-#define factor 15
+#define factor 9
 #define angfac 5
 
 using namespace KDL;
@@ -32,59 +32,54 @@ int main(int argc, char **argv)
 	
 	ros::init(argc, argv, "outpos");
 	ros::NodeHandle nodehandle;
-	ros::Publisher chatter_pub = nodehandle.advertise<tk_arm::outpos>("chatter", 1000);
+	ros::Publisher position_pub = nodehandle.advertise<tk_arm::OutPos>("arm_pos", 0);
 	ros::Rate loop_rate(10);
 
+    Frame pos_goal;
 
-	int count = 0;
-	while (ros::ok())
-	{
-		Frame pos_goal;
+    for(unsigned int i = 0; i < n; i++){
+        double myinput;
+        printf ("pos of joint %i: ",i+1);
+        scanf ("%lf",&myinput);
+        q(i)=myinput*M_PI/180;
+    }
+    
+    fksolver.JntToCart(q,pos_goal);
 
-		for(unsigned int i = 0; i < n; i++){
-			double myinput;
-			printf ("pos of joint %i: ",i+1);
-			scanf ("%lf",&myinput);
-			q(i)=myinput*M_PI/180;
-		}
-		
-		fksolver.JntToCart(q,pos_goal);
+    // for (int i = 0; i < 4; ++i)
+    // {
+    // 	for (int j = 0; j < 4; ++j)
+    // 	{
+    // 		std::cout << "pos_goal:\n" << pos_goal(i,j) << std::endl;
+    // 	}
+    // }
 
-		// for (int i = 0; i < 4; ++i)
-		// {
-		// 	for (int j = 0; j < 4; ++j)
-		// 	{
-		// 		std::cout << "pos_goal:\n" << pos_goal(i,j) << std::endl;
-		// 	}
-		// }
+    int retval;
+    retval = solver.CartToJnt(q_init,pos_goal,q_sol);
 
-		int retval;
-		retval = solver.CartToJnt(q_init,pos_goal,q_sol);
+    // std::cout << "------------------" << std::endl;
+    // std::cout << "ret_val\t" << retval << std::endl;
+    // std::cout << "q_init:\t" << q_init.data.transpose()/M_PI*180.0 << std::endl;
+    std::cout << "pos_goal:\n" << pos_goal << std::endl;
+    // std::cout << "q_sol:\t" << q_sol.data.transpose()/M_PI*180.0 << std::endl;
 
-		// std::cout << "------------------" << std::endl;
-		// std::cout << "ret_val\t" << retval << std::endl;
-		// std::cout << "q_init:\t" << q_init.data.transpose()/M_PI*180.0 << std::endl;
-		std::cout << "pos_goal:\n" << pos_goal << std::endl;
-		// std::cout << "q_sol:\t" << q_sol.data.transpose()/M_PI*180.0 << std::endl;
+    tk_arm::OutPos msg;
 
-		tk_arm::outpos msg;
+    msg.pos1 = q_sol(0);
+    msg.pos2 = q_sol(1);
+    msg.pos3 = q_sol(2);
+    msg.pos4 = q_sol(3);
+    msg.pos5 = 0;
+    msg.pos6 = 0;
 
-		msg.pos1 = q_sol(0);
-		msg.pos2 = q_sol(1);
-		msg.pos3 = q_sol(2);
-		msg.pos4 = q_sol(3);
-		msg.pos5 = 0;
-		msg.pos6 = 0;
-
-		ROS_INFO("OUTPOS:%lf %lf %lf %lf %lf %lf", msg.pos1/M_PI*180.0, msg.pos2/M_PI*180.0, \
-			msg.pos3/M_PI*180.0, msg.pos4/M_PI*180.0, msg.pos5/M_PI*180.0, msg.pos6/M_PI*180.0);
-		chatter_pub.publish(msg);
-
-		ros::spinOnce();
-
-		loop_rate.sleep();
-		++count;
-	}
+    position_pub.publish(msg);
+    ROS_INFO("OUTPOS:%lf %lf %lf %lf %lf %lf", msg.pos1/M_PI*180.0, msg.pos2/M_PI*180.0, \
+        msg.pos3/M_PI*180.0, msg.pos4/M_PI*180.0, msg.pos5/M_PI*180.0, msg.pos6/M_PI*180.0);
+    while(ros::ok())
+    {
+        position_pub.publish(msg);
+        loop_rate.sleep();
+    }
 
 	return 0;
 }
