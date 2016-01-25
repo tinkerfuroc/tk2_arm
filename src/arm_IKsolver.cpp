d496bf4c6039dd2beb95862125a83663e2c5349a
GitHub-mZ <txyzzyl@gmail.com>
2016-01-25 19:32:40 +0800

    camera feedback added

--- src/arm_IKsolver.cpp
+++ src/arm_IKsolver.cpp
@@ -1,37 +1,50 @@
 #include "ros/ros.h"
 #include "tk_arm/OutPos.h"
+#include "tk_arm/TargetFound.h"
 #include <kdl/frames_io.hpp>
 #include <kdl/chainfksolverpos_recursive.hpp>
 #include <kdl/chainiksolverpos_lma.hpp>
 #include <cstdio>
 #include <iostream>
-// #include <sstream>
 
-#define LENGTH_FAC	(10)
-#define ANG_FAC	(0)
-#define RATE (10)
-#define ERR_ITER (10)
-#define INTPOL (1.0/180.0*M_PI)
+static const double LENGTH_FAC = 10.0;
+static const double ANG_FAC = 0.0;
+static const int RATE = 10;
+static const int ERR_ITER = 10;
+static const double INTPOL = 1.0 / 180.0 * M_PI;
+static const double IMAGE_WIDTH = 640.0;
+static const double IMAGE_HEIGHT = 480.0;
+static const double DPOS_FAC = 0.001 * LENGTH_FAC;
+static const double FORWARD_VELO = 0.02;
+static const double SLEEP_TIME = 0.3;
 
-double SEG_MIN[]  = { -77.1	/ 180.0 * M_PI,
-                      26.9	/ 180.0 * M_PI,
-                      24.9	/ 180.0 * M_PI,
-                      -77.1	/ 180.0 * M_PI
-                    };	//min angle pos
+static const double SEG_MIN[] =
+{
+    -77.1   / 180.0 * M_PI,
+    26.9    / 180.0 * M_PI,
+    24.9    / 180.0 * M_PI,
+    -77.1   / 180.0 * M_PI
+};  //min angle pos
 
-double SEG_MAX[]  = {44.1	/ 180.0 * M_PI,
-                     98.1	/ 180.0 * M_PI,
-                     113.1	/ 180.0 * M_PI,
-                     3.1	/ 180.0 * M_PI
-                    };	//max angle pos
+double SEG_MAX[] =
+{
+    44.1    / 180.0 * M_PI,
+    98.1    / 180.0 * M_PI,
+    113.1   / 180.0 * M_PI,
+    3.1     / 180.0 * M_PI
+};  //max angle pos
 
-double SEG_INIT[] = {0		/ 180.0 * M_PI,
-                     67		/ 180.0 * M_PI,
-                     70		/ 180.0 * M_PI,
-                     -30	/ 180.0 * M_PI
-                    };	//init angle pos
+double SEG_INIT[] =
+{
+    0       / 180.0 * M_PI,
+    40      / 180.0 * M_PI,
+    100     / 180.0 * M_PI,
+    -50     / 180.0 * M_PI
+};  //init angle pos
 
 using namespace KDL;
+Frame dpos_old, dpos_new;
+bool updatingpos = false;
 
 bool isAngleOutOfRange(const JntArray q)
 {
@@ -47,14 +60,42 @@ bool isAngleOutOfRange(const JntArray q)
     }
     if (isOut)
     {
-        ROS_INFO("Solution out of range. %d%d%d. POS: %lf %lf %lf. Recalculating...\n", \
+        ROS_INFO("Solution out of range. %d%d%d. POS: %lf %lf %lf. Recalculating...", \
                  label[0], label[1], label[2], \
                  q(0) / M_PI * 180.0, q(1) / M_PI * 180.0, q(2) / M_PI * 180.0);
     }
     return isOut;
 }
 
-void msg_publish(JntArray q, int times, ros::Publisher pub, ros::Rate rate)
+void update_dpos(const tk_arm::TargetFound& msg)
+{
+    if (msg.object_recognized) {
+        updatingpos = true;
+        dpos_new.p(1) = (IMAGE_WIDTH / 2 - msg.object_rect_left) * DPOS_FAC;
+        dpos_new.p(2) = (IMAGE_HEIGHT / 2 - msg.object_rect_top) * DPOS_FAC;
+        updatingpos = false;
+        ROS_INFO("Camera info acquired. Correction: %6.3lf %6.3lf.", dpos_new.p(1), dpos_new.p(2));
+
+        dpos_old = dpos_new;
+    }
+}
+
+// void update_dpos()
+// {
+//     double x, y;
+//     printf("Move Vector X: ");
+//     scanf("%lf", &x);
+//     printf("Move Vector Y: ");
+//     scanf("%lf", &y);
+//     updatingpos = true;
+//     dpos_new.p(1) = (IMAGE_WIDTH / 2 - x) * DPOS_FAC;
+//     dpos_new.p(2) = (IMAGE_HEIGHT / 2 - y) * DPOS_FAC;
+//     updatingpos = false;
+
+//     dpos_old = dpos_new;
+// }
+
+void msg_publish(const JntArray q, const int times, ros::Publisher pub, ros::Rate rate)
 {
     tk_arm::OutPos msg;
 
@@ -65,7 +106,7 @@ void msg_publish(JntArray q, int times, ros::Publisher pub, ros::Rate rate)
     msg.pos5 = 0;
     msg.pos6 = 0;
 
-    ROS_INFO("OUTPOS:%lf %lf %lf %lf %lf %lf", msg.pos1 / M_PI * 180.0, msg.pos2 / M_PI * 180.0, \
+    ROS_INFO("OUTPOS:%6.3lf %6.3lf %6.3lf %6.3lf %6.3lf %6.3lf", msg.pos1 / M_PI * 180.0, msg.pos2 / M_PI * 180.0, \
              msg.pos3 / M_PI * 180.0, msg.pos4 / M_PI * 180.0, msg.pos5 / M_PI * 180.0, msg.pos6 / M_PI * 180.0);
     ros::spinOnce();
 
@@ -89,7 +130,7 @@ void get_pos(Frame &pos)
     for (int i = 0; i < 3 && ros::ok(); ++i)
     {
         double posinput;
-        printf("Coordinate %d: ", i);
+        printf("Coord %d: ", i);
         scanf("%lf", &posinput);
         pos_vec(i) = posinput * LENGTH_FAC;
     }
@@ -98,9 +139,9 @@ void get_pos(Frame &pos)
 
 void chain_init(Chain &chain)
 {
-    chain.addSegment(Segment(Joint(Joint::RotZ), Frame(Vector(0.0, 0.0, 0.01 * LENGTH_FAC))));	//	-77 degree to 44 degree
-    chain.addSegment(Segment(Joint(Joint::RotY), Frame(Vector(0.0, 0.0, 0.35 * LENGTH_FAC))));	// 27 degree to 98 degree
-    chain.addSegment(Segment(Joint(Joint::RotY), Frame(Vector(0.0, 0.0, 0.28 * LENGTH_FAC))));	// 25 degree to 113 degree
+    chain.addSegment(Segment(Joint(Joint::RotZ), Frame(Vector(0.0, 0.0, 0.01 * LENGTH_FAC))));  //  -77 degree to 44 degree
+    chain.addSegment(Segment(Joint(Joint::RotY), Frame(Vector(0.0, 0.0, 0.35 * LENGTH_FAC))));  // 27 degree to 98 degree
+    chain.addSegment(Segment(Joint(Joint::RotY), Frame(Vector(0.0, 0.0, 0.28 * LENGTH_FAC))));  // 25 degree to 113 degree
 }
 
 void arm_init(const Chain chain, JntArray &q_init)
@@ -151,7 +192,8 @@ int solver_judge(const Chain chain, const Eigen::Matrix<double, 6, 1> L, const i
 
     if (errcount == ERR_ITER)
     {
-        ROS_INFO("Max Iteration Reached. No Solution.");
+        ROS_INFO("Max Iteration Reached. No Solution. Cannot reach [%lf, %lf, %lf].", final_pos.p(0) / LENGTH_FAC, \
+                 final_pos.p(1) / LENGTH_FAC, final_pos.p(2) / LENGTH_FAC);
         print_pos(chain, q_init, final_pos);
     }
     else
@@ -177,7 +219,7 @@ int solver_judge(const Chain chain, const Eigen::Matrix<double, 6, 1> L, const i
             msg.pos5 = 0;
             msg.pos6 = 0;
 
-            ROS_INFO("OUTPOS:%lf %lf %lf %lf %lf %lf", msg.pos1 / M_PI * 180.0, msg.pos2 / M_PI * 180.0, \
+            ROS_INFO("OUTPOS: %6.3lf %6.3lf %6.3lf %6.3lf %6.3lf %6.3lf", msg.pos1 / M_PI * 180.0, msg.pos2 / M_PI * 180.0, \
                      msg.pos3 / M_PI * 180.0, msg.pos4 / M_PI * 180.0, msg.pos5 / M_PI * 180.0, msg.pos6 / M_PI * 180.0);
             ros::spinOnce();
 
@@ -202,6 +244,7 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "outpos");
     ros::NodeHandle nodehandle;
     ros::Publisher position_pub = nodehandle.advertise<tk_arm::OutPos>("arm_pos", 0);
+    ros::Subscriber position_sub = nodehandle.subscribe("tk2_vision/arm_target_finder/caught_obj", 10, update_dpos);
     ros::Rate loop_rate(RATE);
 
     //chain definition
@@ -209,28 +252,47 @@ int main(int argc, char **argv)
     chain_init(chain);
 
     int n = chain.getNrOfJoints();
+    double obj_dep;
     Eigen::Matrix<double, 6, 1> L;
     L(0) = L(1) = L(2) = 1;
     L(3) = L(4) = L(5) = ANG_FAC;
-    Frame pos_goal;
     ChainIkSolverPos_LMA solver(chain, L);
-    JntArray q(n), q_init(n), q_init2(n), q_sol(n), q_intpol(n);
+    JntArray q_init(n), q_sol(n);
+    Frame pos_goal, pos_goal_corrected;
 
     arm_init(chain, q_init);
     msg_publish(q_init, 5, position_pub, loop_rate);
     print_pos(chain, q_init, pos_goal);
 
+    printf("Object depth: ");
+    scanf("%lf", &obj_dep);
+    obj_dep *= LENGTH_FAC;
+
     while(ros::ok())
     {
         // pos input
-        get_pos(pos_goal);
+        // get_pos(pos_goal);
+        if (pos_goal.p(0) < obj_dep)
+            pos_goal.p(0) += FORWARD_VELO * LENGTH_FAC;
+
+        // update_dpos();
+
+        if (updatingpos)
+            pos_goal_corrected.p = pos_goal.p + dpos_old.p;
+        else
+            pos_goal_corrected.p = pos_goal.p + dpos_new.p;
+
+        ROS_INFO("Coord after correction: [%6.3lf, %6.3lf, %6.3lf].", pos_goal_corrected.p(0) / LENGTH_FAC, \
+                 pos_goal_corrected.p(1) / LENGTH_FAC, pos_goal_corrected.p(2) / LENGTH_FAC);
 
         //solve IK
         int retval;
-        retval = solver.CartToJnt(q_init, pos_goal, q_sol);
+        retval = solver.CartToJnt(q_init, pos_goal_corrected, q_sol);
 
         //check if solution is valid
-        solver_judge(chain, L, retval, q_init, q_sol, pos_goal, position_pub, loop_rate);
+        solver_judge(chain, L, retval, q_init, q_sol, pos_goal_corrected, position_pub, loop_rate);
+        pos_goal = pos_goal_corrected;
+        ros::Duration(SLEEP_TIME).sleep();
     }
 
     return 0;
