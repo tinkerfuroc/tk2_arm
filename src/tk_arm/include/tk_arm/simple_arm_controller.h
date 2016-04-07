873bc636a804b8d043a10fc4796cf599b6400396
marcus <txyzzyl@gmail.com>
2016-04-08 01:34:44 +0800

    actionlib completed, publisher todo

--- src/tk_arm/include/tk_arm/simple_arm_controller.h
+++ src/tk_arm/include/tk_arm/simple_arm_controller.h
@@ -9,6 +9,7 @@
 #include <kdl/chain.hpp>
 #include <actionlib/server/simple_action_server.h>
 #include <tk_arm/ArmReachObjectAction.h>
+#include <tk_arm/ArmInitAction.h>
 
 namespace tinker
 {
@@ -19,7 +20,6 @@ struct ArmInfo
     std::vector<double> min_angles;
     std::vector<double> max_angles;
     std::vector<double> init_angles;
-    std::vector<double> duck_angles;
     std::vector<KDL::Segment> segments;
 };
 
@@ -28,11 +28,11 @@ class SimpleArmController
 public:
     SimpleArmController(std::string server_name_);
 
+    //callback function for GoToPosition actionlib server
     void PositionCallback(const tk_arm::ArmReachObjectGoalConstPtr &goal);
 
-    void GoInit();
-
-    void PublishCurrentPose();
+    //callback function for GoInit actionlib server
+    void InitCallback(const tk_arm::ArmInitGoalConstPtr &new_goal);
 
     static const int kNumJoint;
     static const int kNumSegment;
@@ -41,47 +41,54 @@ public:
 
     static const double kLengthFactor;
     static const double kAngleFactor;
-    static const double kMoveStep;    
+    static const double kMoveStep;   
+    static const double kDegreeInterpolation; 
 
     static const double kForwardVelocity;
     static const double kBlindDistance;
 
 protected:
-    virtual void SetJointAngle(const KDL::JntArray &joint_angle);
-    virtual bool CalculateJointAngle(const geometry_msgs::Point &target, KDL::JntArray &joint_angle);
-    virtual bool HasArrivedTarget();
+    virtual bool PositionToAngle(const geometry_msgs::Point &target, KDL::JntArray &joint_angle);
+    virtual geometry_msgs::Point AngleToPosition(const KDL::JntArray angle);
+    
     virtual bool GraspObject();
     virtual bool ReleaseObject();
 
-    void GoToPosition();
-    void PublishMissionDone();
+    virtual void PublishCurrentPose();
+    virtual bool GoToPosition();
+    virtual bool GoInit();
 
-    geometry_msgs::Point CalculateEndPostion(const KDL::JntArray angle);
+    bool HasArrivedTarget();
+    bool HasArrivedObject();
+    
     void WarnKDLSolve(int retval);
     bool CheckAngleLegal(const KDL::JntArray &joint_angle);
 
-    ArmInfo arm_info_;
-    KDL::Chain chain_;
-    KDL::JntArray now_joint_angles_;
-    KDL::JntArray target_joint_angles_;
+    ArmInfo arm_info_;  // arm parameters
+    KDL::Chain chain_; // the arm
+
+    // publishers for PublishCurrentPose()
     ros::Publisher position_pub_;
     ros::Publisher mission_pub_;
     ros::Subscriber position_sub_;
 
+    // actionlib servers
     ros::NodeHandle nh_;
     actionlib::SimpleActionServer<tk_arm::ArmReachObjectAction> as_;
+    actionlib::SimpleActionServer<tk_arm::ArmInitAction> as_init_;
     tk_arm::ArmReachObjectResult result_;
+    tk_arm::ArmInitResult result_init_;
     
-    int need_grasp_; //1 to grasp, 2 to release, 0 to do nothing 
+    int need_grasp_; // 1 to grasp, 2 to release, 0 to do nothing 
     bool in_grasp_;
-    bool error_last_time_;
-    bool in_reaching_;
-    bool in_retreiving_;
-    bool in_init_;
 
-    geometry_msgs::Point now_end_point_;
-    geometry_msgs::Point target_end_point_;
-    geometry_msgs::Point object_end_point_;
+    // arm states
+    KDL::JntArray current_joint_angles_;
+    KDL::JntArray target_joint_angles_;
+
+    geometry_msgs::Point current_end_point_;
+    geometry_msgs::Point target_end_point_; // TARGET is the next midway-point for arm
+    geometry_msgs::Point object_end_point_; // OBJECT is the final goal for arm
     geometry_msgs::Point error_;
 };
 }
--- src/tk_arm/src/simple_arm_controller.cc
+++ src/tk_arm/src/simple_arm_controller.cc
@@ -27,6 +27,7 @@ const int SimpleArmController::kGraspWaitTime = 1;
 const double SimpleArmController::kLengthFactor = 10.0;
 const double SimpleArmController::kAngleFactor = 0.0;
 const double SimpleArmController::kMoveStep = 0.005;
+const double SimpleArmController::kDegreeInterpolation = (1.0/180.0*M_PI);
 
 const double SimpleArmController::kForwardVelocity = 0.05;
 const double SimpleArmController::kBlindDistance = 0.05;
@@ -34,17 +35,17 @@ const double SimpleArmController::kBlindDistance = 0.05;
 const double SEG_MIN[]  =
 {
 	-77.1   / 180.0 * M_PI,
-	26.9    / 180.0 * M_PI,
-	24.9    / 180.0 * M_PI,
-	-77.1   / 180.0 * M_PI
+	-0.05235988,
+	0.52359878,
+	-50.0  / 180.0 * M_PI
 };  //min angle pos
 
 const double SEG_MAX[]  =
 {
 	44.1    / 180.0 * M_PI,
-	98.1    / 180.0 * M_PI,
-	113.1   / 180.0 * M_PI,
-	3.1 / 180.0 * M_PI
+	1.6406095,
+	2.3500000,
+	50 / 180.0 * M_PI
 };  //max angle pos
 
 const double SEG_INIT[] =
@@ -57,220 +58,189 @@ const double SEG_INIT[] =
 
 SimpleArmController::SimpleArmController(std::string server_name_) : 
 	as_(nh_, server_name_, boost::bind(&SimpleArmController::PositionCallback, this, _1), false),
-	// action_name_(server_name_),
-	now_joint_angles_(kNumJoint - 1),
-	in_grasp_(2), error_last_time_(false),
-	in_reaching_(false), in_retreiving_(false),
-	in_init_(true)
+	as_init_(nh_, server_name_ + "1", boost::bind(&SimpleArmController::InitCallback, this, _1), false),
+	current_joint_angles_(kNumJoint - 1), target_joint_angles_(kNumJoint - 1), in_grasp_(false)
 {
-	// arm_info_.min_angles.resize(SimpleArmController::kNumJoint);
-	// arm_info_.max_angles.resize(SimpleArmController::kNumJoint);
-	// arm_info_.init_angles.resize(SimpleArmController::kNumJoint);
-	// arm_info_.segments.resize(SimpleArmController::kNumSegment);
-	// for(int i = 0; i < SimpleArmController::kNumJoint; i++)
-	// {
-	// 	arm_info_.min_angles[i] = SEG_MIN[i];
-	// 	arm_info_.max_angles[i] = SEG_MAX[i];
-	// 	arm_info_.init_angles[i] = SEG_INIT[i];
-	// }
+	// load arm parameter into arm_info_
+	arm_info_.min_angles.resize(SimpleArmController::kNumJoint);
+	arm_info_.max_angles.resize(SimpleArmController::kNumJoint);
+	arm_info_.init_angles.resize(SimpleArmController::kNumJoint);
+	arm_info_.segments.resize(SimpleArmController::kNumSegment);
+	for(int i = 0; i < SimpleArmController::kNumJoint; i++)
+	{
+		arm_info_.min_angles[i] = SEG_MIN[i];
+		arm_info_.max_angles[i] = SEG_MAX[i];
+		arm_info_.init_angles[i] = SEG_INIT[i];
+	}
 
-	// arm_info_.segments[0] = Segment(Joint(Joint::RotZ),
-	// 							   Frame(Vector(0.0, 0.0, 0.01 * double(SimpleArmController::kLengthFactor))));
-	// arm_info_.segments[1] = Segment(Joint(Joint::RotY),
-	// 							   Frame(Vector(0.0, 0.0, 0.35 * double(SimpleArmController::kLengthFactor))));
-	// arm_info_.segments[2] = Segment(Joint(Joint::RotY),
-	// 							   Frame(Vector(0.0, 0.0, 0.28 * double(SimpleArmController::kLengthFactor))));
+	arm_info_.segments[0] = Segment(Joint(Joint::RotZ),
+								   Frame(Vector(0.0, 0.0, 0.01 * double(SimpleArmController::kLengthFactor))));
+	arm_info_.segments[1] = Segment(Joint(Joint::RotY),
+								   Frame(Vector(0.0, 0.0, 0.35 * double(SimpleArmController::kLengthFactor))));
+	arm_info_.segments[2] = Segment(Joint(Joint::RotY),
+								   Frame(Vector(0.0, 0.0, 0.28 * double(SimpleArmController::kLengthFactor))));
 
-	// for(int i = 0; i < kNumSegment; i++)
-	// {
-	// 	chain_.addSegment(arm_info_.segments[i]);
-	// }
+	// build the arm model: chain_
+	for(int i = 0; i < kNumSegment; i++)
+		chain_.addSegment(arm_info_.segments[i]);
+
+	for (int i = 0; i < kNumJoint - 1; ++i)
+	{
+		current_joint_angles_(i) = SEG_INIT[i];
+		target_joint_angles_(i) = SEG_INIT[i];
+	}
+
+	ROS_INFO("Arm chain model set. %d segments.", chain_.getNrOfJoints());
+
+	current_end_point_ = AngleToPosition(current_joint_angles_);
 	ROS_INFO("Parameters set.");
+
+	//start actionlib servers
 	as_.start();
-	// GoInit();
+	as_init_.start();
+	ROS_INFO("Server started.");
 }
 
 void SimpleArmController::PositionCallback(const tk_arm::ArmReachObjectGoalConstPtr &new_goal)
 {
-    ROS_INFO("New Goal!");
-    // if (new_goal->pos.x < 0.1 && new_goal->pos.y < 0.1 && new_goal->pos.z < 0.1)
-    //     return;
-  //   if (new_goal->grasp_state == 4)
-  //   {
-	 //    ROS_INFO("Go Init!");
-		// KDL::JntArray angles(kNumJoint - 1);
-		// for(int i = 0; i < kNumJoint - 1; i++)
-		// {
-		// 	angles(i) = arm_info_.init_angles[i];
-		// }
-		// geometry_msgs::Point point = CalculateEndPostion(angles);
-		// object_end_point_ = point;
-		// result_.is_reached = true;
-		// as_.setSucceeded(result_);
-  //   	return;
-  //   }
+	ROS_ASSERT(new_goal->grasp_state <= 3 && new_goal->grasp_state >= 0);
+    ROS_INFO("New Goal! [%4.2lf %4.2lf %4.2lf]:%s", new_goal->pos.x, new_goal->pos.y, new_goal->pos.z, 
+    	new_goal->grasp_state == 2 ? "release" : new_goal->grasp_state == 1 ? "grasp" : "keep_state");
+
 	object_end_point_ = new_goal->pos;
 	need_grasp_ = new_goal->grasp_state;
-	result_.is_reached = true;
-	as_.setSucceeded(result_);
+    if (HasArrivedObject())
+    {
+		ROS_INFO("No move needed.");
+		switch (new_goal->grasp_state)
+		{
+			case 1: GraspObject(); break;
+			case 2: ReleaseObject(); break;
+			default: break;
+		}
+		result_.is_reached = true;
+		as_.setSucceeded(result_);		
+		ROS_INFO("Action Succeded.");
+    }
+    else
+    {
+		result_.is_reached = GoToPosition();
+		if (!result_.is_reached)
+		{
+			ROS_INFO("Go to position failed.");
+			as_.setAborted(result_);
+		ROS_INFO("Action Aborted.");
+			return;
+		}
+		ROS_INFO("Go to position succeded.");
+		switch (new_goal->grasp_state)
+		{
+			case 1: GraspObject(); break;
+			case 2: ReleaseObject(); break;
+			default: break;
+		}
+		as_.setSucceeded(result_);
+		ROS_INFO("Action Succeded.");
+	}
 }
 
-void SimpleArmController::GoInit()
+void SimpleArmController::InitCallback(const tk_arm::ArmInitGoalConstPtr &new_goal)
 {
-    ROS_INFO("Go Init!");
-	KDL::JntArray angles(kNumJoint - 1);
+	ROS_INFO("Go Init! [%d]", new_goal->state);
+	result_init_.is_reached = GoInit();
+	ROS_INFO("%s", result_init_.is_reached ? "Go init success." : "Go init failed.");
+	as_init_.setSucceeded(result_init_);
+}
+
+bool SimpleArmController::GoInit()
+{
+	// set goal to init angles
 	for(int i = 0; i < kNumJoint - 1; i++)
 	{
-		angles(i) = arm_info_.init_angles[i];
+		target_joint_angles_(i) = arm_info_.init_angles[i];
 	}
-	geometry_msgs::Point point = CalculateEndPostion(angles);
-	object_end_point_ = point;
-	GoToPosition();
+	target_end_point_ = AngleToPosition(target_joint_angles_);
+
+	// interpolation in motor angle
+	double maxDegree = 0.0;
+	int interpolationNum = 0.0;
+	for (int i = 0; i < 3; ++i)
+	{
+	    if (fabs(target_joint_angles_(i) - current_joint_angles_(i)) > maxDegree)
+	        maxDegree = fabs(target_joint_angles_(i) - current_joint_angles_(i));
+	}
+	interpolationNum = maxDegree / kDegreeInterpolation + 1;
+    std::vector<double> msg;
+    msg.resize(kNumJoint);
+    KDL::JntArray initial_joint_angles_(current_joint_angles_);
+	for (int i = 0; i < interpolationNum; ++i)
+	{
+		ros::Rate rate(10);
+	    current_joint_angles_(0) = (target_joint_angles_(0) * i + initial_joint_angles_(0) * (interpolationNum - i)) / interpolationNum;
+	    current_joint_angles_(1) = (target_joint_angles_(1) * i + initial_joint_angles_(1) * (interpolationNum - i)) / interpolationNum;
+		current_joint_angles_(2) = (target_joint_angles_(2) * i + initial_joint_angles_(2) * (interpolationNum - i)) / interpolationNum;
+
+	    PublishCurrentPose();
+	    ros::spinOnce();
+	    rate.sleep();
+	}
+	current_joint_angles_(0) = target_joint_angles_(0);
+	current_joint_angles_(1) = target_joint_angles_(1);
+	current_joint_angles_(2) = target_joint_angles_(2);
+    PublishCurrentPose();	
+
+	return true;
 }
 
-void SimpleArmController::GoToPosition()
+bool SimpleArmController::GoToPosition()
 {
-	result_.is_reached = true;
-	as_.setSucceeded(result_);
+	// interpolate on current-to-object direction
+	bool is_ok = true;
+    std::vector<double> msg;
+    msg.resize(kNumJoint);
+	while (!HasArrivedObject())
+	{
+		ros::Rate rate(10);
+		double x = object_end_point_.x - current_end_point_.x;
+		double y = object_end_point_.y - current_end_point_.y;
+		double z = object_end_point_.z - current_end_point_.z;
+		double distance = sqrt(x * x + y * y + z * z);
+		target_end_point_.x = current_end_point_.x + x * kMoveStep / distance;
+		target_end_point_.y = current_end_point_.y + y * kMoveStep / distance;
+		target_end_point_.z = current_end_point_.z + z * kMoveStep / distance;
+		if (!PositionToAngle(target_end_point_, target_joint_angles_))
+		{
+			ROS_WARN("Position to angle failed.");
+			is_ok = false;
+			break;
+		}
+
+	    current_joint_angles_ = target_joint_angles_;
+	    PublishCurrentPose();
+	    ros::spinOnce();
+	    rate.sleep();
+	}
+	return is_ok;
 }
 
 void SimpleArmController::PublishCurrentPose()
 {
+	ROS_INFO("\033[0;34mPublishing angle: %5.2lf %5.2lf %5.2lf\033[0;0m", current_joint_angles_(0)*180/M_PI, current_joint_angles_(1)*180/M_PI, 
+		current_joint_angles_(2)*180/M_PI);
+	current_end_point_ = AngleToPosition(current_joint_angles_);
 	// tk_arm::OutPos msg;
-	// msg.pos1 = now_joint_angles_(0);
-	// msg.pos2 = now_joint_angles_(1);
-	// msg.pos3 = now_joint_angles_(2);
+	// msg.pos1 = current_joint_angles_(0);
+	// msg.pos2 = current_joint_angles_(1);
+	// msg.pos3 = current_joint_angles_(2);
 	// msg.pos4 = M_PI / 2 - msg.pos2 - msg.pos3;
 	// msg.pos5 = 0;
 	// msg.pos6 = in_grasp_;
 	// position_pub_.publish(msg);
 }
 
-void SimpleArmController::PublishMissionDone()
+bool SimpleArmController::PositionToAngle(const geometry_msgs::Point &target, KDL::JntArray &joint_angle)
 {
- //    std_msgs::String msg;
- //    ROS_ERROR("PublishMissionDone!");
-	// msg.data = "Done.";
-	// mission_pub_.publish(msg);	
-}
-
-// bool SimpleArmController::TimeCallback()
-// {
-// 	if (error_last_time_)
-// 	{
-// 		ROS_WARN("Cannot reach");
-//         GoInit();
-//         need_grasp_ = false;
-//         in_reaching_ = false;
-//         in_retreiving_ = false;
-// 		in_init_ = true;
-//         in_duck_ = false;
-//         error_last_time_ = false;
-// 	}
-// 	if (HasArrivedTarget())
-// 	{
-//         if (in_init_)
-// 		{
-// 			if(need_grasp_ == true)
-// 			{
-//                 ROS_ERROR("Need grasp!");
-//                 waiting_target_ = false;
-// 				in_init_ = false;
-// 				in_reaching_ = true;
-// 			}
-//             else
-//             {
-//                 if(!waiting_target_)
-//                     PublishMissionDone();
-//                 waiting_target_ = true;
-//             }
-//             ROS_WARN("Init target: %lf %lf %lf", object_end_point_.x, object_end_point_.y, object_end_point_.z);
-// 		}
-// 		else if(in_reaching_)
-// 		{
-// 			geometry_msgs::Point point = target_end_point_;
-// 			if (!aligned_with_object_)
-// 			{
-//                 double factor = now_end_point_.x / object_end_point_.x;
-// 				point.y = object_end_point_.y * factor;
-// 				point.z = object_end_point_.z * factor;
-// 				SetTarget(point);
-// 				aligned_with_object_ = true;
-// 			}
-// 			else if (point.x < object_end_point_.x)
-// 			{
-// 				if (point.x < object_end_point_.x - kBlindDistance)
-// 				{
-// 					point.x += kForwardVelocity;
-//                     double factor = point.x / object_end_point_.x;
-//                     point.y = object_end_point_.y * factor;
-//                     point.z = object_end_point_.z * factor;
-// 					SetTarget(point);
-// 				}
-// 				else
-// 				{
-// 					printf("correction count: %d\n", correction_updated_count_);
-// 					if (correction_updated_count_ >= kCorrectionCount)
-// 					{
-// 						point.x = object_end_point_.x;
-// 						SetTarget(point);
-// 					}
-// 					else
-// 					{
-// 						GoInit();
-//                         need_grasp_ = false;
-// 						in_reaching_ = false;
-// 						in_init_ = true;
-// 						aligned_with_object_ = false;                            	
-// 					}
-// 				}
-// 			}
-// 			else
-// 			{
-// 				GraspObject();
-// 				GoInit();
-//                 need_grasp_ = false;
-// 				in_reaching_ = false;
-// 				in_retreiving_ = true;
-// 				aligned_with_object_ = false;
-// 			}
-// 		}
-// 		else if(in_retreiving_)
-// 		{
-// 			ReleaseObject();
-//             GoInit();
-//             need_grasp_ = false;
-// 			in_retreiving_ = false;
-// 			in_init_ = true;
-// 		}
-// 	}
-// 	BaseSimpleArmController::TimeCallback();
-// }
-
-// bool SimpleArmController::GetNewTarget(geometry_msgs::Point &new_target)
-// {
-// 	double x = target_end_point_.x - now_end_point_.x;
-// 	double y = target_end_point_.y - now_end_point_.y;
-// 	double z = target_end_point_.z - now_end_point_.z;
-// 	double distance = sqrt(x * x + y * y + z * z);
-// 	new_target.x = now_end_point_.x + x * kMoveStep / distance;
-// 	new_target.y = now_end_point_.y + y * kMoveStep / distance;
-// 	new_target.z = now_end_point_.z + z * kMoveStep / distance;
-// 	return true;
-// }
-
-void SimpleArmController::SetJointAngle(const KDL::JntArray &joint_angle)
-{
-	// need_start_ = false;
-	PublishCurrentPose();
-	now_joint_angles_ = joint_angle;
-	now_end_point_ = CalculateEndPostion(now_joint_angles_);
-}
-
-bool SimpleArmController::CalculateJointAngle(const geometry_msgs::Point &target, KDL::JntArray &joint_angle)
-{
-	double x = target_end_point_.x - now_end_point_.x;
-	Eigen::Matrix < double, 2 * (kNumJoint - 1), 1 > constraint;
+	Eigen::Matrix<double, 2*(kNumJoint - 1), 1> constraint;
 	for(int i = 0; i < kNumJoint - 1; i++)
 	{
 		constraint(i) = 1;
@@ -278,14 +248,14 @@ bool SimpleArmController::CalculateJointAngle(const geometry_msgs::Point &target
 	}
 	KDL::ChainIkSolverPos_LMA solver(chain_, constraint);
 	KDL::JntArray new_joint_angles(kNumJoint - 1);
-	KDL::JntArray seed_joint_angles = now_joint_angles_;
+	KDL::JntArray initial_joint_angles_ = current_joint_angles_;
 	int retval = -1;
 	KDL::Frame pos_goal(KDL::Vector(target.x * kLengthFactor,
 									target.y * kLengthFactor,
 									target.z * kLengthFactor));
 	for(int i = 0; i < kErrorRetry; i++)
 	{
-		retval = solver.CartToJnt(seed_joint_angles, pos_goal, new_joint_angles);
+		retval = solver.CartToJnt(initial_joint_angles_, pos_goal, new_joint_angles);
 		if(retval != 0)
 		{
 			WarnKDLSolve(retval);
@@ -294,24 +264,30 @@ bool SimpleArmController::CalculateJointAngle(const geometry_msgs::Point &target
 		if(CheckAngleLegal(new_joint_angles))
 		{
 			joint_angle = new_joint_angles;
-			error_last_time_ = false;
 			return true;
 		}
-		seed_joint_angles.data.setRandom();
-		seed_joint_angles.data *= M_PI;
+		initial_joint_angles_.data.setRandom();
+		initial_joint_angles_.data *= M_PI;
 	}
 	ROS_ERROR("failed to calculate joint angle");
-	ROS_ERROR("target position %6.3lf, %6.3lf, %6.3lf", target.x, target.y, target.z);
-	ROS_INFO("now position %6.3lf, %6.3lf, %6.3lf", now_end_point_.x, now_end_point_.y, now_end_point_.z);
-	error_last_time_ = true;
+	ROS_ERROR("target position %5.2lf, %5.2lf, %5.2lf", target.x, target.y, target.z);
+	ROS_INFO("now position %5.2lf, %5.2lf, %5.2lf", current_end_point_.x, current_end_point_.y, current_end_point_.z);
 	return false;
+}
+
+bool SimpleArmController::HasArrivedObject()
+{
+	error_.x = current_end_point_.x - object_end_point_.x;
+	error_.y = current_end_point_.y - object_end_point_.y;
+	error_.z = current_end_point_.z - object_end_point_.z;
+	return sqrt(error_.x * error_.x + error_.y  * error_.y  + error_.z  * error_.z ) < 0.01;
 }
 
 bool SimpleArmController::HasArrivedTarget()
 {
-	error_.x = now_end_point_.x - target_end_point_.x;
-	error_.y = now_end_point_.y - target_end_point_.y;
-	error_.z = now_end_point_.z - target_end_point_.z;
+	error_.x = current_end_point_.x - target_end_point_.x;
+	error_.y = current_end_point_.y - target_end_point_.y;
+	error_.z = current_end_point_.z - target_end_point_.z;
 	return sqrt(error_.x * error_.x + error_.y  * error_.y  + error_.z  * error_.z ) < 0.01;
 }
 
@@ -322,11 +298,11 @@ bool SimpleArmController::GraspObject()
 		ros::Duration(kGraspWaitTime).sleep();
 		in_grasp_ = true;
 		PublishCurrentPose();
-		ROS_INFO("Target grasped!");
+		ROS_INFO("\033[1;35mObject grasped!\033[0;0m");
 		ros::Duration(kGraspWaitTime).sleep();
 		return true;
 	}
-	return false;
+	return true;
 }
 
 bool SimpleArmController::ReleaseObject()
@@ -336,14 +312,14 @@ bool SimpleArmController::ReleaseObject()
 		ros::Duration(kGraspWaitTime).sleep();
 		in_grasp_ = false;
 		PublishCurrentPose();
-		ROS_INFO("Target released!");
+		ROS_INFO("\033[1;35mObject released!\033[0;0m");
 		ros::Duration(kGraspWaitTime).sleep();
 		return true;
 	}
-	return false;
+	return true;
 }
 
-geometry_msgs::Point SimpleArmController::CalculateEndPostion(const KDL::JntArray angle)
+geometry_msgs::Point SimpleArmController::AngleToPosition(const KDL::JntArray angle)
 {
 	KDL::ChainFkSolverPos_recursive fksolver(chain_);
 	KDL::Frame pos;
@@ -352,7 +328,7 @@ geometry_msgs::Point SimpleArmController::CalculateEndPostion(const KDL::JntArra
 	end_point.x = pos.p.x() / double(kLengthFactor);
 	end_point.y = pos.p.y() / double(kLengthFactor);
 	end_point.z = pos.p.z() / double(kLengthFactor);
-	ROS_INFO("now end position: %6.3lf, %6.3lf, %6.3lf", end_point.x, end_point.y, end_point.z);
+	ROS_INFO("\033[0;36mCurrent Position: %5.2lf %5.2lf %5.2lf\033[0;0m", end_point.x, end_point.y, end_point.z);
 	return end_point;
 }
 
