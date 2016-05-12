52557666697779ecb64392c509f8ec5a217e7595
marcus <txyzzyl@gmail.com>
2016-05-12 22:28:04 +0800

    [ADD]Overall point actionlib

--- tk_arm/script/arm_planner.py
+++ tk_arm/script/arm_planner.py
@@ -2,10 +2,12 @@
 
 import actionlib
 import rospy
-from tk_arm.msg._ArmReachObjectGoal import ArmReachObjectGoal
-from tk_arm.msg._ArmReachObjectFeedback import ArmReachObjectFeedback
-from tk_arm.msg._ArmReachObjectResult import ArmReachObjectResult
-from tk_arm.msg._ArmReachObjectAction import ArmReachObjectAction
+from tk_arm.msg._ArmPathGoal import ArmPathGoal
+from tk_arm.msg._ArmPathFeedback import ArmPathFeedback
+from tk_arm.msg._ArmPathResult import ArmPathResult
+from tk_arm.msg._ArmPathAction import ArmPathAction
+# from tk_arm.msg._SimpleMoveGoal import SimpleMoveGoal
+# from tk_arm.msg._SimpleMoveAction import SimpleMoveAction
 from tinker_msgs.msg._SimpleMoveGoal import SimpleMoveGoal
 from tinker_msgs.msg._SimpleMoveAction import SimpleMoveAction
 import tf
@@ -17,67 +19,68 @@ class ArmPlanAction:
     def __init__(self, name):
         self.action_name = name
         self._as = actionlib.SimpleActionServer(self.action_name,
-                ArmReachObjectAction,
+                ArmPathAction,
                 execute_cb=self.execute_cb,
                 auto_start = False)
-        self._feedback = ArmReachObjectFeedback()
-        self._result = ArmReachObjectResult()
+        self._feedback = ArmPathFeedback()
+        self._result = ArmPathResult()
         self.trans = tf.TransformListener()
         self.chassis_move_client = actionlib.SimpleActionClient('simple_move', SimpleMoveAction)
         #self.chassis_move_client.wait_for_server()
-        self.arm_move_client = actionlib.SimpleActionClient('arm_reach_position', ArmReachObjectAction)
+        self.arm_move_client = actionlib.SimpleActionClient('arm_path', ArmPathAction)
         self.arm_move_client.wait_for_server()
         rospy.loginfo('Planner start')
         self._as.start()
 
     def execute_cb(self, goal):
-        goal_pos_stamped = goal.pos
-        goal_point = goal_pos_stamped
-        rospy.loginfo('target raw %f %f %f', goal_point.point.x, 
-                goal_point.point.y, goal_point.point.z)
-        if goal_pos_stamped.header.frame_id in ArmPlanAction._static_frame_ids:
-            goal_pos_stamped.header.stamp = rospy.Time(0)
+        goal_point = goal.path.poses[-1]
+        # rospy.loginfo('target raw %f %f %f', goal_point.point.x, 
+        #         goal_point.point.y, goal_point.point.z)
+        if goal_point.header.frame_id in ArmPlanAction._static_frame_ids:
+            goal_point.header.stamp = rospy.Time(0)
         while not rospy.is_shutdown():
             try:
                 goal_point = self.trans.transformPoint('arm_origin_link', 
-                    goal_pos_stamped)
+                    goal_point)
                 break
             except Exception as e:
                 rospy.logwarn(e.message)
-        origin_goal = goal_point.point
-        rospy.loginfo('target %f %f %f', goal_point.point.x, 
-                goal_point.point.y, goal_point.point.z)
-        # first move chassis over y
-        if (fabs(goal_point.point.y) > 0.03):
-            chassis_result = self.move_chassis(0, goal_point.point.y, 0)
-            if not chassis_result.success:
-                rospy.logwarn('failed to move chassis')
-            goal_point.point.y -= chassis_result.moved_distance.y
-        # now move arm
-        rospy.loginfo('arm %f %f %f', goal_point.point.x, 
+        # origin_goal = goal.path
+        # rospy.loginfo('target %f %f %f', goal_point.point.x, 
+        #         goal_point.point.y, goal_point.point.z)
+        # # first move chassis over y
+        # if (fabs(goal_point.point.y) > 0.03):
+        #     chassis_result = self.move_chassis(0, goal_point.point.y, 0)
+        #     if not chassis_result.success:
+        #         rospy.logwarn('failed to move chassis')
+        #     goal_point.point.y -= chassis_result.moved_distance.y
+        # Move arm first
+        rospy.loginfo('arm goal %f %f %f', goal_point.point.x, 
                 goal_point.point.y, goal_point.point.z)
         arm_goal = goal
-        arm_goal.pos = goal_point
-        arm_goal.grasp_state &= 0x01 # do not force grasp in case of failure
+        arm_goal.path = goal.path
+        # arm_goal.grasp_state &= 0x01 # do not force grasp in case of failure
         self.arm_move_client.send_goal(arm_goal)
         self.arm_move_client.wait_for_result()
         arm_result = self.arm_move_client.get_result()
+        rospy.loginfo('arm moved %4.2f %4.2f %4.2f', arm_result.moved.x, 
+                arm_result.moved.y, arm_result.moved.z)
         goal_point.point.x -= arm_result.moved.x
         goal_point.point.y -= arm_result.moved.y
         goal_point.point.z -= arm_result.moved.z
-        rospy.loginfo('arm moved %f %f %f', arm_result.moved.x, 
-                arm_result.moved.y, arm_result.moved.z)
         self._result.is_reached = arm_result.is_reached
-        # finally move chassis over x
+        # finally move chassis over x and y
         if not arm_result.is_reached:
-            rospy.loginfo('chassis move x %f', goal_point.point.x)
-            chassis_result = self.move_chassis(goal_point.point.x, 0, 0)
+            rospy.loginfo('chassis move [x,y] = [%4.2f,%4.2f]', 
+                    goal_point.point.x, goal_point.point.y)
+            chassis_result = self.move_chassis(goal_point.point.x, goal_point.point.y, 0)
             goal_point.point.x -= chassis_result.moved_distance.x
+            goal_point.point.y -= chassis_result.moved_distance.y
             self._result.is_reached = chassis_result.success
-            arm_goal = goal
-            arm_goal.pos.point = arm_result.moved
-            self.arm_move_client.send_goal(arm_goal)
-            self.arm_move_client.wait_for_result()
+        #     arm_goal = goal
+        #     arm_goal.pos.point = arm_result.moved
+        #     self.arm_move_client.send_goal(arm_goal)
+        #     self.arm_move_client.wait_for_result()
         self._result.moved = arm_result.moved
         if self._result.is_reached:
             self._as.set_succeeded(self._result)
--- tk_arm/src/test_plan.cpp
+++ tk_arm/src/test_plan.cpp
@@ -1,6 +1,8 @@
 #include "tk_arm/base_astar_planner.h"
+#include <actionlib/server/simple_action_server.h>
 #include <actionlib/client/simple_action_client.h>
 #include <actionlib/client/terminal_state.h>
+#include <tk_arm/ArmReachObjectAction.h>
 #include <tk_arm/ArmPathAction.h>
 #include <cstdio>
 
@@ -9,54 +11,63 @@ using namespace tinker::arm;
 bool success;
 tk_arm::ArmPathGoal goal;
 
-void sendMsg(actionlib::SimpleActionClient<tk_arm::ArmPathAction> &ac);
-
-int main(int argc, char *argv[]) {
-    ros::init(argc, argv, "test_plan");
-    actionlib::SimpleActionClient<tk_arm::ArmPathAction> ac("arm_path", true);
-    ROS_INFO("Waiting for action server to start.");
-    ac.waitForServer();
-    ROS_INFO("Action server started.");
-
+namespace tinker{
+namespace arm{
+class ArmPointServer
+{
+protected:
+    ros::NodeHandle nh_;
+    actionlib::SimpleActionServer<tk_arm::ArmReachObjectAction> as_;
+    actionlib::SimpleActionClient<tk_arm::ArmPathAction> ac_;
+    tk_arm::ArmReachObjectResult result_;
     BaseAStarPlanner astar_planner;
-    geometry_msgs::Point p[4];
-    p[0].x = 0.5;
-    p[0].y = 0;
-    p[0].z = 0;
+    geometry_msgs::Point p0, p1;
 
-    p[1].x = 0.8;
-    p[1].y = 0.1;
-    p[1].z = 0.3;
+public:
+    ArmPointServer():
+        nh_(), as_(nh_, "arm_point", boost::bind(&ArmPointServer::PointCallback, this, _1), false),
+        ac_("arm_path", true)
+    {
+        p0.x = 0.38;
+        p0.y = -0.02;
+        p0.z = 0.15;
+        ROS_INFO("Waiting for action server to start.");
+        as_.start();
+        ac_.waitForServer();
+        ROS_INFO("Action server started.");
+    }
 
-    p[2].x = 0.1;
-    p[2].y = 0.2;
-    p[2].z = -0.5;
-
-    p[3].x = 0.64;
-    p[3].y = 0;
-    p[3].z = 0.8;
-
-    goal.path = astar_planner.GetPath(p[0], p[1], success);
-    sendMsg(ac);
-    goal.path = astar_planner.GetPath(p[0], p[2], success);
-    sendMsg(ac);
-    goal.path = astar_planner.GetPath(p[1], p[2], success);
-    sendMsg(ac);
-    goal.path = astar_planner.GetPath(p[3], p[2], success);
-    sendMsg(ac);
-
-    return 0;
+    ~ArmPointServer()
+    {
+    }
+    void PointCallback(const tk_arm::ArmReachObjectGoalConstPtr &new_goal)
+    {
+        p1 = new_goal->pos.point;
+        goal.path = astar_planner.GetPath(p0, p1, success);
+        ac_.sendGoal(goal);
+        
+        ROS_INFO("Waiting for result...");
+        bool finished_before_timeout = ac_.waitForResult();
+        if (finished_before_timeout)
+        {
+            actionlib::SimpleClientGoalState state = ac_.getState();
+            ROS_INFO("Action finished: %s", state.toString().c_str());
+            p0 = p1;
+        }
+        else
+        {
+            ROS_INFO("Action did not finish before the time out.");
+            p0 = ac_.getResult()->moved;
+        }
+    }
+};
+}
 }
 
-void sendMsg(actionlib::SimpleActionClient<tk_arm::ArmPathAction> &ac){    
-    getchar();
-    ac.sendGoal(goal);
-
-    ROS_INFO("Waiting for result...");
-    bool finished_before_timeout = ac.waitForResult();
-    if (finished_before_timeout) {
-        actionlib::SimpleClientGoalState state = ac.getState();
-        ROS_INFO("Action finished: %s", state.toString().c_str());
-    } else
-        ROS_INFO("Action did not finish before the time out.");
+int main(int argc, char *argv[])
+{
+    ros::init(argc, argv, "test_plan");
+    ArmPointServer as;
+    ros::spin();
+    return 0;
 }
